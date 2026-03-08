39445d78e7fec865043d88be87f22a20af70675d
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2026-03-08 14:27:13 +0530

    Starting ReactJS

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>APP</h1>
+        <h3>Hello World</h3>
+      </div>
+      <div className="newdiv">
+        <h1>APP</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
