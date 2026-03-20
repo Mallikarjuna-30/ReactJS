1bccc8f87aaeb63ecdc46e059dd160f676973238
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2026-03-20 12:24:55 +0530

    feat: Intro to tailwind css

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='h-screen w-full bg-gray-900'>
+        <h1 className='text-3xl font-bold text-white'>Hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
