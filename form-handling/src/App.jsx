ce25f45b84a2cae5d6e582a66170381e3db0596f
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2026-03-22 15:50:36 +0530

    feat: Form Handling

---
+++ form-handling/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  const formHandler = (e) => {
+    e.preventDefault()
+    console.log("Form submitted")
+  }
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        formHandler(e)
+      }}>
+        <input type="text" placeholder='Enter here ' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
