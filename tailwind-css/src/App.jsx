caa4feee9df901690188e4cf4c516781220d007d
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2026-04-05 15:17:28 +0530

    feat: implement dark mode toggle functionality with App and Navbar components

--- tailwind-css/src/App.jsx
+++ tailwind-css/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import { useState } from 'react'
 
 const App = () => {
+  const [dark, setDark] = useState(0)
   return (
     <>
-      <div className='h-screen w-full bg-black'>
-        <Navbar />
+      <div className={`h-screen w-full ${dark ? 'bg-black' : 'bg-white'}`} style={{
+        backgroundColor: dark ? 'black' : 'white',
+        color: dark ? 'white' : 'black'
+      }}>
+        <Navbar dark={dark} setDark={setDark} />
         <div>
-          <h1 className='text-3xl font-bold text-white px-4 py-2'>Hello world!</h1>
+          <h1 className='text-3xl font-bold px-4 py-2' style={{ color: dark ? 'white' : 'black' }}>Hello world!</h1>
         </div>
       </div>
     </>
--- tailwind-css/src/Components/Navbar.jsx
+++ tailwind-css/src/Components/Navbar.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
 
-const Navbar = () => {
-    const [dark, setDark] = useState(0)
+const Navbar = (props) => {
+
 
     return (
         <div className='flex justify-between items-center bg-gray-500 px-4 py-2'>
-            <div className='text-white text-xl'>logo</div>
+            <div
+                style={{
+                    color: props.dark ? 'white' : 'black'
+                }}
+                className='text-white text-xl'>logo</div>
             <button
                 onClick={() => {
-                    setDark(!dark)
-                }}
-                style={{
-                    backgroundColor: dark ? 'black' : 'white',
-                    color: dark ? 'white' : 'black'
+                    props.setDark(!props.dark)
                 }}
                 className='text-white border-black px-2 py-2'>
-                <h2>{dark ? 'Dark Mode' : 'Light Mode'}</h2>
+                <h2
+                    style={{
+                        color: props.dark ? 'white' : 'black'
+
+                    }}
+                >{props.dark ? 'Light Mode' : 'Dark Mode'}</h2>
             </button>
         </div>
     )
